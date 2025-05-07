6ecb6ab1c3d8da833cbca376bd300613bf2c1aa1
adarshmane007 <121188276+adarshmane007@users.noreply.github.com>
2025-05-08 00:52:27 +0530

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "us-east-1"  # Change this to your preferred region
+}
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name22"  # Replace with a globally unique name
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
